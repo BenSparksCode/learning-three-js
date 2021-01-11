785a9fb15dc30f1ea765b078c4587e3366b383e4
Ben Sparks <bensparks0@gmail.com>
2021-01-11 21:49:13 +0200

    Added spawn and click physics

--- src/App.js
+++ src/App.js
@@ -1,15 +1,39 @@
-
 import { Canvas } from 'react-three-fiber'
-import {OrbitControls, Stars} from 'drei'
+import { OrbitControls, Stars } from 'drei'
+import { Physics, useBox, usePlane } from 'use-cannon'
 import './App.css';
 
-function Box() {
-   return ( 
-     <mesh>
-       <boxBufferGeometry attach='geometry' />
-       <meshLambertMaterial attach='material' color='hotpink' />
-     </mesh>
-   )
+function Box(props) {
+  const [ref, api] = useBox(() => ({
+    mass: 1,
+    position: [0,5,0],
+    ...props
+  }))
+
+  return (
+    <mesh 
+    onClick={()=>{
+      api.velocity.set(0,2,0)
+    }}
+    ref={ref} position={[0, 2, 0]} >
+      <boxBufferGeometry attach='geometry' />
+      <meshLambertMaterial attach='material' color='hotpink' />
+    </mesh>
+  )
+}
+
+function Plane(props) {
+  const [ref] = usePlane(() => ({
+    rotation: [-Math.PI / 2, 0, 0],
+    ...props
+  }))
+
+  return (
+    <mesh ref={ref} position={[0, 0, 0]}>
+      <planeBufferGeometry attach='geometry' args={[300, 300]} />
+      <meshLambertMaterial attach='material' color='lightblue' />
+    </mesh>
+  )
 }
 
 function App() {
@@ -19,10 +43,13 @@ function App() {
       <Stars />
       <ambientLight intensity={0.5} />
       <spotLight
-          position={[10, 15, 10]}
-          angle={0.3}
-        />
-      <Box />
+        position={[10, 15, 10]}
+        angle={0.3}
+      />
+      <Physics>
+        <Box />
+        <Plane />
+      </Physics>
     </Canvas>
   );
 }
